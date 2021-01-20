fa82bf6b6390182e3f8f857ef7a27439992731e0
unknown <bhaskarnavuluri9@gmail.com>
2021-01-20 21:56:47 +0530

    added arthmetic operation

--- uc1.sh
+++ uc1.sh
@@ -4,3 +4,6 @@
 		read -p "Enter first input :" a
 		read -p "Enter second input:" b
 		read -p "Enter third input :" c
+	echo "Result:" $result1
+result2=$(($a * $b + $c));
+         echo "Result2" $result2
